171f8cb9352f480b9e1cc06c805e125ed82889f1
马兆铿 <810768333@qq.com>
2019-04-08 10:30:57 +0800

    🌟：【设计】增加 Promise 链例子。

--- 设计-Promise链.js
+++ 设计-Promise链.js
@@ -1,31 +1,74 @@
 /*----------------------------------------------------------------------------------
-* about:Promise链生成
+* about:Promise链生成。形态与 axios 一致。
 * date:2018-12-25
 * ----------------------------------------------------------------------------------*/
 
-function Man( str ) {
-  this.say = function () {
-    const promise = new Promise(( resolve, reject ) => {
-      if ( typeof str === "string" ) {
-        console.log(str);
-        resolve(str);
-      } else {
-        reject("not string");
-      }
-    });
-    return promise;
-  }
+function Fetch( config ) {
+  console.log("开始发送，配置：", config);
+  return new Promise(( resolve, reject ) => {
+    if ( config ) {
+      resolve("请求发送成功")
+    } else {
+      reject("请求发送失败");
+    }
+  });
 }
 
-const man1 = new Man("1");
-const man2 = new Man("2");
-const man3 = new Man("3");
-const man4 = new Man("4");
-const man5 = new Man("5");
+// 模拟普通请求
+function fetch( str ) {
+  return new Fetch(str);
+}
 
-let promise = Promise.resolve({});
-const res = promise
-  .then(man1.say, man2.say)
-  .then(man5.say, undefined)
-  .then(man3.say, man4.say);
+// axios请求链
+const intercepter = {
+  request: {
+    success( config ) {
+     config && console.log("请求拦截，配置：", config);
+      config.requestIntercepter = true;
+      if ( config.trigErr === "requestIntercepter" ) {
+        return Promise.reject("请求拦截错误");
+      }
+      return config; // 作为下一个 Promise 的参数
+    },
+    fail( config ) {
+      return Promise.reject("请求拦截，错误：", config);
+    }
+  },
+  responce: {
+    success( res ) {
+      console.log("响应拦截，结果：", res);
+      return res;
+    },
+    fail( err ) {
+      return Promise.reject("响应拦截，错误：", err);
+    }
+  }
+};
 
+function chainFetch( config ) {
+  console.log("/*----------------------------------*/");
+  config && console.log(config.name);
+  let chain = Promise.resolve(config); // 参数传递
+  // 增加拦截
+  chain = chain
+    .then(intercepter.request.success, intercepter.request.fail) // 请求拦截
+    .then(fetch, undefined) // 请求
+    .then(intercepter.responce.success, intercepter.responce.fail) // 响应拦截
+  return chain;
+}
+
+chainFetch({ name: "请求链1：全部resolve" })
+  .then(res => {
+    console.log("请求链全部完成，结果：", res);
+  })
+  .catch(err => {
+    console.log("请求链出现错误，错误：", err);
+  });
+
+/*chainFetch({ name: "请求链2：请求拦截中错误", trigErr: "requestIntercepter" })
+  .then(res => {
+    console.log("请求链全部完成，结果：", res);
+  })
+  .catch(err => {
+    console.log("请求链出现错误，错误：", err);
+  });*/
